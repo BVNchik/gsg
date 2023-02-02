be8f3879beac7f5ff2e2b8ac7e7e6c574bacfa68
BVNchik <bulanov.vladislav32@gmail.com>
2023-02-02 16:25:15 +0300

    Settings app

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-# Description
+# GSG
 
-A public service for providing the opportunity to fill out various forms (for example: Claim filing) to any user.
+## **`Github Statistics Generator`**
 
-Written in React
+## Pre-requirements
 
-# Installation
+- `node` - see `.node-version` for version. Also please use node version manager like `asdf` or `nodenv`
 
-- install node. For version see `.node-version` file
+- `yarn`
+
+  - Make sure your yarn version is < 2
+
+    > yarn -v
+
+## Installation
+
+- install node
 
 - install dependencies, while in the root of the repo
 
   > yarn
+
+- Create `.env` file for every application in apps folder
+
+- Add variables to env file. Refer to `.env.example` of an application.
 
 - Run server, while in the root of the repo
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,51 @@
-"use strict";
-
-const path = require("path");
-const dotenv = require("dotenv");
-const dotenvExpand = require("dotenv-expand");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+/* eslint-disable @typescript-eslint/no-var-requires */
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const dotenv = require("dotenv");
+const dotenvExpand = require("dotenv-expand");
 const Dotenv = require("dotenv-webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
 
 module.exports = () => {
-  console.log("Webpack is running", { test: process.env });
+  console.log("Webpack is running");
+  const isProduction = process.env.NODE_ENV === "production";
   return {
-    mode: "development",
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? false : "cheap-module-source-map",
     devServer: {
-      hot: true,
-      port: parseInt(process.env.PORT) || 3004,
-      historyApiFallback: true,
-      compress: true,
       allowedHosts: [".localhost"],
       static: {
         directory: path.resolve(__dirname, "static"),
       },
+      hot: true,
+      port: parseInt(process.env.PORT) || 3004,
+      historyApiFallback: true,
+      compress: true,
     },
     entry: "./src/index.tsx",
     output: {
       path: path.resolve(__dirname, "build"),
-      filename: "bundle.js",
+      // Do not use hashes in development: https://github.com/webpack/webpack/issues/10796
+      filename: isProduction
+        ? "static/js/bundle-[fullhash].js"
+        : "static/js/bundle-[name].js",
+      chunkFilename: isProduction
+        ? "static/js/[chunkhash].chunk.js"
+        : "static/js/[name].chunk.js",
     },
     resolve: {
       modules: [path.join(__dirname, "src"), "node_modules"],
       alias: {
         react: path.join(__dirname, "node_modules", "react"),
+        components: path.resolve(__dirname, "src/components/"),
+        pages: path.resolve(__dirname, "src/pages/"),
+        Graphql: path.resolve(__dirname, "src/graphql/"),
+        lib: path.resolve(__dirname, "src/lib/"),
+        utils: path.resolve(__dirname, "src/utils/"),
       },
       extensions: [".tsx", ".ts", ".js"],
     },
@@ -46,15 +58,8 @@ module.exports = () => {
           options: {
             cacheDirectory: true,
             plugins: [
-              require.resolve("react-refresh/babel"), // only for dev
+              !isProduction && require.resolve("react-refresh/babel"),
             ].filter(Boolean),
-          },
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
           },
         },
         {
@@ -65,6 +70,41 @@ module.exports = () => {
             },
             {
               loader: "css-loader",
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "static/media/[name].[contenthash].[ext]",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                svgoConfig: {
+                  plugins: [
+                    {
+                      name: "removeViewBox",
+                      active: false,
+                    },
+                  ],
+                },
+              },
+            },
+            {
+              loader: "file-loader",
+              options: {
+                name: "static/media/[name].[hash:8].[ext]",
+              },
             },
           ],
         },
